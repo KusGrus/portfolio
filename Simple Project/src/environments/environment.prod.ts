3c7d88a8dee3355f1c1e0d3a7f49448e4fb5fafe
Vladislav <vladkusgrus@yandex.ru>
2020-12-14 15:10:28 +0500

    add

---
+++ Simple Project/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAmc4cupwxtJio8jA1QhOoMY1AcfCXMDVs',
+  fbDbUrl: 'https://angular-block-e8685.firebaseio.com'
+};
